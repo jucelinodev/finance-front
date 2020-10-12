c79d38183682ef1e11a9ca9a98cb1c7c8ab3e34e
Jucelino Novais <jucelinodev@gmail.com>
2020-10-12 15:26:20 -0300

    adds axios interceptor to handle connection error

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,19 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001'
-})
-export default api
+  baseURL: "http://localhost:3001",
+});
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (!error.response) {
+      alert("Erro de conexão com o servidor.");
+    }
+
+    return Promise.reject(error);
+  }
+);
+export default api;
